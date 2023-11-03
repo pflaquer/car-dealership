833b5c13237a5987cd418d0bbd06b5be7032149b
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-11-03 16:15:00 -0400

    Update MainMenu.tsx

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -37,7 +37,7 @@ function MainMenu() {
               sx={{
                 height: "2rem",
                 flexGrow:{md: 3.4, lg: 2},
-                bgcolor: "#000000",
+                bgcolor: "gray",
                 clipPath: "polygon(0 0, 100% 0, 96% 100%, 0% 100%)",
                 mr: {md: 0, lg: -5},
                 px: 4,
