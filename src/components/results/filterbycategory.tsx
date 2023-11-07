7d2c0bc1efeb79d0acaeea2999f4c219dbe89190
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-11-07 15:01:33 -0500

    Create filterbycategory.tsx

---
+++ src/components/results/filterbycategory.tsx
@@ -0,0 +1,53 @@
+import { FC, useState, useEffect } from 'react'; 
+
+interface car {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+}
+
+const cars: car[] = [
+  { id: 1, name: 'Tesla Model Y', price: 1000, category: 'suv' },
+  { id: 2, name: 'Lamborghini Urus', price: 500, category: 'suv' },
+  { id: 3, name: 'Toyota 4 Door', price: 20, category: 'sedan' },
+  { id: 4, name: 'Honda 4 Door', price: 50, category: 'sedan' },
+  { id: 5, name: 'Nissan 350Z Drop Top', price: 15, category: 'convertible' },
+];
+
+const FilterByCategory: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [filteredProducts, setFilteredProducts] = useState<car[]>([]);
+
+  const handleCategoryChange = (event: any) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  useEffect(() => {
+    if (selectedCategory === '') {
+      setFilteredProducts(cars);
+    } else {
+      const filteredProducts = cars.filter((car) => car.category === selectedCategory);
+      setFilteredProducts(filteredProducts);
+    }
+  }, [selectedCategory, cars]);
+
+  return (
+    <div>
+      <button value="suv" onClick={handleCategoryChange}>SUV </button>
+      <button value="sedan" onClick={handleCategoryChange}>Sedan </button>
+      <button value="convertible" onClick={handleCategoryChange}>Convertible </button>
+      
+      
+      <ul>
+        {filteredProducts.map((car) => (
+          <li key={car.id}>
+            {car.name} - ${car.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FilterByCategory;
